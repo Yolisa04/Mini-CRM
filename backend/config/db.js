5e8b0a3d61e1bcfcc29f061a2b16deabf645ff6f
Jaric <yolisahlohla@gmail.com>
2026-07-29 01:13:09 +0200

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`MongoDB connected: ${conn.connection.host}`);
-  return conn;
+  try {
+    const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/miniCRM";
+    const conn = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`❌ Error: ${err.message}`);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
